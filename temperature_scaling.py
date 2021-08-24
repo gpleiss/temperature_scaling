126a50975e0cd76c0496c44080bcaf7defca42b3
Geoff Pleiss <gpleiss@gmail.com>
2021-08-24 07:37:01 -0400

    Zero gradient in LBFGS optimizer

--- temperature_scaling.py
+++ temperature_scaling.py
@@ -60,6 +60,7 @@ class ModelWithTemperature(nn.Module):
         optimizer = optim.LBFGS([self.temperature], lr=0.01, max_iter=50)
 
         def eval():
+            optimizer.zero_grad()
             loss = nll_criterion(self.temperature_scale(logits), labels)
             loss.backward()
             return loss
